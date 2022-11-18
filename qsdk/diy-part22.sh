69cb560eb590a1af456dbe628e70d6a0b0ed367f
sadasdasasd <32637304+avijxxx@users.noreply.github.com>
2022-11-19 01:01:44 +0800

    Update diy-part22.sh

--- qsdk/diy-part22.sh
+++ qsdk/diy-part22.sh
@@ -4,12 +4,8 @@ sed -i 's/SSID1=Redmi_AX6000_2G/SSID1=123123_2.4/g' package/mtk/luci-app-mtwifi/
 sed -i 's/WPAPSK1=12345678/WPAPSK1=88896825/g' package/mtk/luci-app-mtwifi/files/mt7615.1.dat
 sed -i 's/WPAPSK1=12345678/WPAPSK1=88896825/g' package/mtk/luci-app-mtwifi/files/mt7615.2.dat
 
-#rm -rf feeds/packages/lang/golang
-#svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
-#git clone https://github.com/sbwml/luci-app-alist package/alist
+
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 git clone https://github.com/sirpdboy/netspeedtest package/netspeedtest
-#KMS
-#git clone https://github.com/cokebar/luci-app-vlmcsd.git package/luci/luci-app-vlmcsd
-#git clone https://github.com/cokebar/openwrt-vlmcsd.git package/luci/openwrt-vlmcsd
+
 
