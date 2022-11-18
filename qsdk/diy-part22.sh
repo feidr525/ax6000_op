2f62931591a6d9e4aa28a8b532a6709bf137989a
sadasdasasd <32637304+avijxxx@users.noreply.github.com>
2022-11-18 16:03:09 +0800

    Update diy-part22.sh

--- qsdk/diy-part22.sh
+++ qsdk/diy-part22.sh
@@ -1,1 +1,3 @@
-
+sed -i 's/192.168.6.1/192.168.10.12/g' package/base-files/files/bin/config_generate
+sed -i 's/SSID1=Redmi_AX6000_5G/SSID1=123123/g' package/mtk/luci-app-mtwifi/files/mt7615.2.dat
+sed -i 's/SSID1=Redmi_AX6000_2G/SSID1=123123_2.4/g' package/mtk/luci-app-mtwifi/files/mt7615.1.dat
