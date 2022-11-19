ace2f8e981ee9d16704d031de1279ee0cf7413a0
sadasdasasd <32637304+avijxxx@users.noreply.github.com>
2022-11-19 21:03:35 +0800

    Update diy-part22.sh

--- qsdk/diy-part22.sh
+++ qsdk/diy-part22.sh
@@ -1,8 +1,8 @@
-sed -i 's/192.168.6.1/192.168.10.12/g' package/base-files/files/bin/config_generate
-sed -i 's/SSID1=Redmi_AX6000_5G/SSID1=123123/g' package/mtk/luci-app-mtwifi/files/mt7615.2.dat
-sed -i 's/SSID1=Redmi_AX6000_2G/SSID1=123123_2.4/g' package/mtk/luci-app-mtwifi/files/mt7615.1.dat
-sed -i 's/WPAPSK1=12345678/WPAPSK1=88896825/g' package/mtk/luci-app-mtwifi/files/mt7615.1.dat
-sed -i 's/WPAPSK1=12345678/WPAPSK1=88896825/g' package/mtk/luci-app-mtwifi/files/mt7615.2.dat
+sed -i 's/192.168.6.1/192.168.1.1253/g' package/base-files/files/bin/config_generate
+sed -i 's/SSID1=Redmi_AX6000_5G/SSID1=openwrt/g' package/mtk/luci-app-mtwifi/files/mt7615.2.dat
+sed -i 's/SSID1=Redmi_AX6000_2G/SSID1=openwrt_2.4/g' package/mtk/luci-app-mtwifi/files/mt7615.1.dat
+sed -i 's/WPAPSK1=12345678/WPAPSK1=password/g' package/mtk/luci-app-mtwifi/files/mt7615.1.dat
+sed -i 's/WPAPSK1=12345678/WPAPSK1=password/g' package/mtk/luci-app-mtwifi/files/mt7615.2.dat
 
 
 #增加mosdns
@@ -21,12 +21,12 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luc
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 
-
+#移动openvpn到vpn
 sed -i 's/services/vpn/g' package/feeds/luci/luci-app-openvpn/luasrc/controller/*.lua
 sed -i 's/services/vpn/g' package/feeds/luci/luci-app-openvpn/luasrc/model/cbi/*.lua
 sed -i 's/services/vpn/g' package/feeds/luci/luci-app-openvpn/luasrc/view/openvpn/*.htm
 
-
+#移动测速到服务
 sed -i 's/network/services/g' package/netspeedtest/luci-app-netspeedtest/luasrc/controller/*.lua
 sed -i 's/network/services/g' package/netspeedtest/luci-app-netspeedtest/luasrc/model/cbi/netspeedtest/*.lua
 sed -i 's/network/services/g' package/netspeedtest/luci-app-netspeedtest/luasrc/view/netspeedtest/*.htm
